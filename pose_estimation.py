67ccbbc71295b16799655987c78b19f0ef0fb836
lioravd <113434611+lioravd@users.noreply.github.com>
2023-05-14 19:04:51 +0300

    Add files via upload

--- pose_estimation.py
+++ pose_estimation.py
@@ -36,6 +36,23 @@ from pycoral.adapters import common
 from pycoral.utils.edgetpu import make_interpreter
 import numpy as np
 _NUM_KEYPOINTS = 17
+nose = 0
+leftEye = 1
+rightEye = 2
+leftEar = 3
+rightEar = 4
+leftShoulder = 5
+rightShoulder = 6
+leftElbow = 7
+rightElbow = 8
+leftWrist = 9
+rightWrist = 10
+leftHip = 11
+rightHip = 12
+leftKnee = 13
+rightKnee = 14
+leftAnkle = 15
+rightAnkle = 16
 
 RED = (200,20,20)
 
