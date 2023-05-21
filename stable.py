afca9c26754303d91e2449f857ad09b31f4b9078
lioravd <113434611+lioravd@users.noreply.github.com>
2023-05-21 09:23:37 +0300

    Add files via upload

--- stable.py
+++ stable.py
@@ -273,7 +273,7 @@ while True :
 
     surf = pygame.surfarray.make_surface(frame[-1::-1])
     screen.blit(text, baloon.x)
-    screen.blit(pygame.transform.scale(surf, (400, 400)), (0, 0))
+    screen.blit(pygame.transform.scale(surf, (300, 300)), (0, 0))
 
     pygame.display.flip()
 
@@ -283,6 +283,9 @@ while True :
             baloon.v += [-4,2]
         if event.type == KEYDOWN and event.key == K_d:
             baloon.v += [4,2]
+        if event.type == KEYDOWN and event.key == K_p:
+            pygame.image.save(screen, "screenshot.jpeg")
+
 
 
 
