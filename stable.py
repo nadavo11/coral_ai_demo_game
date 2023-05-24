26f334734825828c30b597eaabdd7456ab474127
nadavo11 <45533190+nadavo11@users.noreply.github.com>
2023-05-24 12:10:05 +0300

    Add files via upload

--- stable.py
+++ stable.py
@@ -137,7 +137,7 @@ class Baloon():
 
 
 def line(p1, p2):
-    pygame.draw.line(screen, WHITE, (int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1])),width=3)
+    pygame.draw.line(screen, WHITE, (int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1])),2)
 
 
 def draw_body(pose):
