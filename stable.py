d9e3214d5b4e7af48a620c1b59036a09b7030a4f
nadavo11 <45533190+nadavo11@users.noreply.github.com>
2023-05-28 16:22:45 +0300

    Add files via upload

--- stable.py
+++ stable.py
@@ -15,7 +15,7 @@ os.environ["DISPLAY"] = ":0"
 flags = FULLSCREEN | DOUBLEBUF
 
 # Set up the display
-width, height = 1900, 900
+width, height = 1920, 1080
 
 
 screen = pygame.display.set_mode((width, height))
